f57c60699d8eeec1f285cfe3d4102a121e85714a
Evgeny Ebergard <eugeneebergard@gmail.com>
2020-08-17 15:12:06 +0500

    added html

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   entry: {
-    main: './src/index.js',
+    main: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
