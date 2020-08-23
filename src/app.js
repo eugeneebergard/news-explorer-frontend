e808995f69a57ed1950d7afd8bfb75fe5d4d9150
Evgeny Ebergard <eugeneebergard@gmail.com>
2020-08-24 01:24:31 +0500

    created popups and added tablet adaptive

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
 import './style.css';
+
+const popupAuth = document.querySelector('.popup_type_auth');
+const popupSignup = document.querySelector('.popup_type_signup');
+
+const popupLinkSignup = document.querySelector('.popup__link_signup');
+const popupLinkAuth = document.querySelector('.popup__link_auth');
+
+const buttonOpenAuth = document.querySelector('.header__button_auth');
+
+const buttonCloseAuth = document.querySelector('.popup__close_type_auth');
+const buttonCloseSignup = document.querySelector('.popup__close_type_signup');
+
+buttonOpenAuth.addEventListener('click', () => {
+  popupAuth.classList.add('popup_is-opened')
+});
+
+popupLinkSignup.addEventListener('click', () => {
+  popupAuth.classList.remove('popup_is-opened')
+  popupSignup.classList.add('popup_is-opened')
+});
+
+popupLinkAuth.addEventListener('click', () => {
+  popupSignup.classList.remove('popup_is-opened')
+  popupAuth.classList.add('popup_is-opened')
+});
+
+buttonCloseAuth.addEventListener('click', () => {
+  popupAuth.classList.remove('popup_is-opened')
+});
+
+buttonCloseSignup.addEventListener('click', () => {
+  popupSignup.classList.remove('popup_is-opened')
+});
+
+
