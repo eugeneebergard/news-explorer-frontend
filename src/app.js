47bf53cac992521eecb3e7e7f4beb5d49f488fa7
Evgeny Ebergard <eugeneebergard@gmail.com>
2020-08-24 14:28:24 +0500

    backup before reset header

--- src/app.js
+++ src/app.js
@@ -11,26 +11,44 @@ const buttonOpenAuth = document.querySelector('.header__button_auth');
 const buttonCloseAuth = document.querySelector('.popup__close_type_auth');
 const buttonCloseSignup = document.querySelector('.popup__close_type_signup');
 
+const buttonOpenMenu = document.querySelector('.header__mobile-menu_open');
+const buttonCloseMenu = document.querySelector('.header__mobile-menu_close');
+
+const mobileMenu = document.querySelector('.header__mobile-menu');
+const desktopMenu = document.querySelector('.header__border');
+
 buttonOpenAuth.addEventListener('click', () => {
-  popupAuth.classList.add('popup_is-opened')
+  popupAuth.classList.add('popup_is-opened');
 });
 
 popupLinkSignup.addEventListener('click', () => {
-  popupAuth.classList.remove('popup_is-opened')
-  popupSignup.classList.add('popup_is-opened')
+  popupAuth.classList.remove('popup_is-opened');
+  popupSignup.classList.add('popup_is-opened');
 });
 
 popupLinkAuth.addEventListener('click', () => {
-  popupSignup.classList.remove('popup_is-opened')
-  popupAuth.classList.add('popup_is-opened')
+  popupSignup.classList.remove('popup_is-opened');
+  popupAuth.classList.add('popup_is-opened');
 });
 
 buttonCloseAuth.addEventListener('click', () => {
-  popupAuth.classList.remove('popup_is-opened')
+  popupAuth.classList.remove('popup_is-opened');
 });
 
 buttonCloseSignup.addEventListener('click', () => {
-  popupSignup.classList.remove('popup_is-opened')
+  popupSignup.classList.remove('popup_is-opened');
 });
+
+buttonOpenMenu.addEventListener('click', () => {
+  mobileMenu.classList.add('header__mobile-menu_show');
+  desktopMenu.classList.add('header__border_hidden');
+});
+
+buttonCloseMenu.addEventListener('click', () => {
+  mobileMenu.classList.remove('header__mobile-menu_show');
+  desktopMenu.classList.remove('header__border_hidden');
+});
+
+
 
 
